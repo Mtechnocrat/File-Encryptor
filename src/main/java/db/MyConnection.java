59b3a5c838ad6690adaf9692a17f5525a0b68adb
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-09-10 00:56:28 +0530

    Connection with server complete

---
+++ src/main/java/db/MyConnection.java
@@ -0,0 +1,35 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection=null;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/File_Encryptor?useSSL=false", "root", "12345678");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Connection is established");
+        return connection;
+    }
+    public static void  closeConnection(){
+        if(connection!=null){
+            try {
+                connection.close();
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyConnection.getConnection();
+    }
+}
