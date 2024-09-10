6932304824010056cb07966e17c1765febf29612
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-09-10 20:02:13 +0530

    User Authentication added

---
+++ src/main/java/service/SendOTPService.java
@@ -0,0 +1,66 @@
+package service;
+
+import  jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendOTPService {
+    public static void sendOTP(String email, String genOTP) {
+        // Recipient's email ID
+        String to = email;
+
+        // Sender's email ID (your email)
+        String from = "aayush8090kumar@gmail.com";
+
+        // Gmail SMTP server
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the Session object and pass your Gmail credentials
+        Session session = Session.getInstance(properties,new jakarta.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, "oywbbketxcoapgmu");
+            }
+        });
+
+        // Used to debug SMTP issues
+        session.setDebug(true);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("File Enc OTP");
+
+            // Set the actual message
+            message.setText("Your One time Password for File Enc app is " + genOTP);
+
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
--- src/main/java/views/Welcome.java
+++ src/main/java/views/Welcome.java
@@ -1,36 +1,92 @@
 package views;
 
+import dao.UserDAO;
+import model.User;
+import service.SendOTPService;
+import service.UserService;
+import service.generateOTP;
+
+import javax.sql.rowset.spi.SyncResolver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Welcome {
-        public void WelcomeScreen(){
-            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Welcome to the App : 😊");
-            System.out.println("Press 1 :  Login");
-            System.out.println("Press 2 : Sign Up");
-            System.out.println("Press 0 : Exit 😠");
-            System.out.print("Enter your choice : ");
+    public void WelcomeScreen() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome to the App : 😊");
+        System.out.println("Press 1 :  Login");
+        System.out.println("Press 2 : Sign Up");
+        System.out.println("Press 0 : Exit 😠");
+        System.out.print("Enter your choice : ");
 
-            int choice=0;
-            try {
-                choice=Integer.parseInt(br.readLine());
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-
-            switch (choice){
-                case 1->login();
-                case 2->signUp();
-                case 0->System.exit(0);
-            }
+        int choice = 0;
+        try {
+            choice = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-    private void login() {
-
+        switch (choice) {
+            case 1 -> login();
+            case 2 -> signUp();
+            case 0 -> System.exit(0);
+        }
     }
+
+    private void login() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Email : ");
+        String email = sc.nextLine();
+        try {
+            if (UserDAO.isExists(email)) {
+                String genOTP = generateOTP.getOTP();
+                SendOTPService.sendOTP(email, genOTP);
+
+                System.out.print("Enter your OTP : ");
+                String otp = sc.nextLine();
+                if (otp.equals(genOTP)) {
+                    System.out.println("Welcome");
+                } else {
+                    System.out.println("Wrong OTP");
+                }
+            } else {
+                System.out.println("User don't exists");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void signUp() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter name :");
+        String name = sc.nextLine();
+
+        System.out.print("Enter email address : ");
+        String email= sc.nextLine();
+
+        //Checking for the mail is vaild or not
+        String genOTP = generateOTP.getOTP();
+        SendOTPService.sendOTP(email, genOTP);
+
+        System.out.print("Enter your OTP");
+        String otp= sc.nextLine();
+
+        if (otp.equals(genOTP)) {
+            User user=new User(name,email);
+            int response= UserService.saveUser(user);
+            switch (response){
+                case '0'-> System.out.println("User Registered");
+                case '1'-> System.out.println("User already exist");
+
+            }
+        } else {
+            System.out.println("Wrong OTP");
+        }
 
     }
 
